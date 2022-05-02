6219c52a0c82d84e3c1b826b0b9900db55d64649
Julia Gomes <magalhaesjulia@id.uff.br>
2022-05-02 18:02:20 -0300

    Init commit

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import { Widget } from './components/Widget';
+
+export function App() {
+  return <Widget />
+}
+
+// interface ButtonProps {
+//   text: string;
+// }
+
+// function Button(props: ButtonProps) {
+//   return <button className="bg-black">{props.text ?? 'Padraozinho'}</button>
+// }
+
+// function App() {
+//   return(
+//     <div>
+//       <Button text="enviar"/>
+//       <Button text="ok"/>
+//       <Button/>
+//     </div>
+//   )
+// }
+
+// export default App
---
+++ src/components/Widget.tsx
@@ -0,0 +1,24 @@
+import { ChatTeardropDots } from "phosphor-react";
+import { useState } from "react";
+
+export function Widget() {
+  const [isWidgetOpen, setIsWidgetOpen] = useState(false)
+
+  function toggleWidgetVisibility() {
+    setIsWidgetOpen(!isWidgetOpen)
+  }
+
+  return (
+    <div className="absolute bottom-5 right-5">
+      { isWidgetOpen && <p>oi</p> }
+
+      <button onClick={toggleWidgetVisibility} className="group flex items-center bg-brand-500 rounded-full px-3 h-12 text-white">
+        <ChatTeardropDots className="w-6 h-6"/>
+        <span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-300 ease-linear">
+          <span className="pl-2"></span>
+          Feedback
+        </span>
+      </button>
+    </div>
+  )
+}
