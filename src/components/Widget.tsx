44735639d332a28143d98dacb65bd50d53e26bbd
Julia Gomes <magalhaesjulia@id.uff.br>
2022-05-03 23:24:35 -0300

    Update Widget component importing Widget Form

--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -1,10 +1,13 @@
 import { ChatTeardropDots } from 'phosphor-react';
 import { Popover } from '@headlessui/react';
+import { WidgetForm } from './WidgetForm';
 
 export function Widget() {
   return (
-    <Popover className="absolute bottom-5 right-5">
-      <Popover.Panel>oi</Popover.Panel>
+    <Popover className="absolute bottom-4 right-4 md:bottom-10 md:right-10 flex flex-col items-end">
+      <Popover.Panel>
+        <WidgetForm/>
+      </Popover.Panel>
 
       <Popover.Button className="group flex items-center bg-brand-500 rounded-full px-3 h-12 text-white">
         <ChatTeardropDots className="w-6 h-6"/>
