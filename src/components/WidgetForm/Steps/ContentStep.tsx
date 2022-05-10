acf64b351b580170f8f4c0380325c313d5dd72b2
Julia Gomes <magalhaesjulia@id.uff.br>
2022-05-10 14:44:34 -0300

    Implements behavior on screenshot button

--- src/components/WidgetForm/Steps/ContentStep.tsx
+++ src/components/WidgetForm/Steps/ContentStep.tsx
@@ -1,4 +1,5 @@
 import { ArrowLeft } from "phosphor-react";
+import { useState } from "react";
 import { FeedbackType, feedbackTypes } from ".."
 import { CloseButton } from "../../CloseButton"
 import { ScreenshotButton } from "../ScreenshotButton";
@@ -9,6 +10,7 @@ interface ContentStepProps {
 }
 
 export function ContentStep({ feedbackType, onFeedbackRestartRequested }: ContentStepProps) {
+  const [screenshot, setScreenshot] = useState<string | null >(null);
   const feedbackTypeInfo = feedbackTypes[feedbackType];
 
   return(
@@ -37,7 +39,10 @@ export function ContentStep({ feedbackType, onFeedbackRestartRequested }: Conten
         />
 
         <footer className="flex gap-2 mt-2">
-          <ScreenshotButton/>
+          <ScreenshotButton
+            screenshot={screenshot}
+            onScreenshotTaken={setScreenshot}
+          />
 
           <button
             type="submit"
