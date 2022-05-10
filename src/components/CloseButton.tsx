552edc7f394db59bff1ea1d5c58a7964207af246
Julia Gomes <magalhaesjulia@id.uff.br>
2022-05-10 15:31:16 -0300

    Adds focus style to close button

--- src/components/CloseButton.tsx
+++ src/components/CloseButton.tsx
@@ -3,7 +3,7 @@ import { X } from 'phosphor-react';
 
 export function CloseButton() {
   return(
-    <Popover.Button className="top-5 right-5 absolute text-zinc-400 hover:text-zinc-100" title="Fechar formulário de feedback">
+    <Popover.Button className="top-5 right-5 absolute text-zinc-400 hover:text-zinc-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500" title="Fechar formulário de feedback">
       <X weight="bold" className="w-4 h-4"/>
     </Popover.Button>
   );
